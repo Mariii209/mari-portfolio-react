672ab1b10305fcc27aa6fdce4c574fd3e277153f
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-05-06 10:50:30 -0700

    Imported Roboto from Google Fonts and did some styling on the SiteMenu

--- src/SiteMenu.js
+++ src/SiteMenu.js
@@ -4,12 +4,28 @@ import "./SiteMenu.css";
 export default function SiteMenu() {
   return (
     <div className="SiteMenu">
-      <h3 className="SiteTitle">Maritza Gutierrez</h3>
-      <ul className="NavList">
-        <li className="NavItem">Home</li>
-        <li className="NavItem">About Me</li>
-        <li className="NavItem">Projects</li>
-        <li className="NavItem">Contact</li>
+      <h3 className="BrandName">Maritza Gutierrez</h3>
+      <ul className="MenuList">
+        <li className="MenuItem">
+          <a href="/" className="MenuLink active">
+            Home
+          </a>
+        </li>
+        <li className="MenuItem">
+          <a href="/about" className="MenuLink">
+            About Me
+          </a>
+        </li>
+        <li className="MenuItem">
+          <a href="/projects" className="MenuLink">
+            Projects
+          </a>
+        </li>
+        <li className="MenuItem">
+          <a href="/contact" className="MenuLink">
+            Contact
+          </a>
+        </li>
       </ul>
     </div>
   );
