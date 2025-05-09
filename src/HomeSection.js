6014cd45d2f19ac3b967ac1f939681dc313353b0
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-05-08 17:53:50 -0700

    Reapply "Restoring"
    This reverts commit 60600fb30d4ce63fb652e0817117264ddb6544db.

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./HomeSection.css";
 import Profile from "./Profile.js";
+<<<<<<< Updated upstream
 import SiteMenu from "./SiteMenu";
 import { NavLink } from "react-router-dom";
+=======
+import AboutMe from "./AboutMe";
+>>>>>>> Stashed changes
 
 export default function HomeSection() {
   return (
     <div className="HomeSection">
+<<<<<<< Updated upstream
       <SiteMenu />
       <div className="HomeContent">
         <Profile />
@@ -31,6 +36,29 @@ export default function HomeSection() {
             </li>
           </ul>
         </div>
+=======
+      <Profile />
+      <div className="MeetMaritza">
+        <h1 className="GreetingTitle">Hello!</h1>
+        <h2 className="IntroSubtitle">Who is Maritza Gutierrez?</h2>
+        <ul className="IntroNav">
+          <li className="IntroNavItem">
+            <a href="/AboutMe" className="IntroNavLink">
+              About Me
+            </a>
+          </li>
+          <li className="IntroNavItem">
+            <a href="/" className="IntroNavLink">
+              Projects
+            </a>
+          </li>
+          <li className="IntroNavItem">
+            <a href="/" className="IntroNavLink">
+              Contact
+            </a>
+          </li>
+        </ul>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
