e670d20a627eb2af455c930765d3741979a48c82
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-05-11 15:09:03 -0700

    Got Web3Form keys and hooked up the form to it

--- src/Contact.js
+++ src/Contact.js
@@ -3,12 +3,34 @@ import SiteMenu from "./SiteMenu";
 import "./Contact.css";
 
 export default function Contact() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "e32146bc-af1d-40dd-9267-c98c6f84b02b");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
   return (
     <div className="Contact">
       <SiteMenu />
 
       <section className="ContactForm">
-        <form>
+        <form onSubmit={onSubmit}>
           <h2>Contact Me</h2>
           <div className="InboxInfo">
             <label>Full Name</label>
@@ -16,6 +38,7 @@ export default function Contact() {
               type="text"
               className="Field"
               placeholder="Enter your name"
+              name="name"
               required
             />
           </div>
@@ -25,14 +48,14 @@ export default function Contact() {
               type="email"
               className="Field"
               placeholder="Enter your email"
+              name="email"
               required
             />
           </div>
           <div className="InboxInfo">
             <label>Your Message</label>
             <textarea
-              name=""
-              id=""
+              name="message"
               className="Field Mess"
               placeholder="Enter your message"
               required
