11f59d0de9d628ee77c4123de6f557b849c18bc0
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-05-15 08:37:27 -0700

    Add responsive footer for HomeSection and adjust visibility for screens < 768px
    - Added a secondary footer to the bottom of the HomeSection.
    - Set default display to none; becomes visible when screen width is < 768px.
    - Hid the ProfileFooter and some additional content on smaller screens for cleaner mobile layout.
    - Used CSS media queries to manage visibility based on screen size.

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -34,6 +34,25 @@ export default function HomeSection() {
           </ul>
         </div>
       </div>
+      <div className="HomeFooter">
+        <ul>
+          <li>
+            <a href="/">
+              <i className="fa-brands fa-linkedin FooterIcons"></i>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <i className="fa-regular fa-envelope FooterIcons"></i>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <i className="fa-brands fa-github FooterIcons"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
